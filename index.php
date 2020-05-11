713d3b771a6edd7fc7a009379a981c351c1ededb
Dafs26 <dafz260@gmail.com>
2020-05-10 22:50:26 -0400

    v1.0

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=100%, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/indexstyle.css">
+    <title>Aperture Laboratories</title>
+</head>
+<body>
+<header>
+    <img id="logo" src="imagenes/logo4.png" alt="">
+    
+</header>
+<nav>
+    <ul>
+        <li class="navbar"><a href="#Home">Home</a></li>
+        <li class="navbar"><a href="#History">History</a></li>
+        <li class="navbar"><a href="#Contact Us">Contact Us</a></li>
+    </ul>
+</nav>
+<div id="content">
+<section>
+    <h1>Aperture Science Inc.</h1><hr />
+    <p id="introduccion">Las instalaciones de Aperture son un enorme complejo dividido básicamente en dos partes: las instalaciones antiguas puestas en operación desde los años 1950 hasta alrededor de los años 1980 y las instalaciones nuevas que funcionan desde la década de 1990.
+
+La primera es la más profunda y llegan a una profundidad de alrededor de 5 km bajo la superficie y se encuentran construidas en lo que antiguamente fue una mina de sal, que fue adquirida por el fundador Cave Johnson. Estas instalaciones cuentan con varios tiros dentro de los cuales se encuentran unas estructuras geodésicas dentro de las cuales se construyen cámaras de prueba para analizar diversos tipos de inventos; estas estructuras se encuentran en grupos de 9 por cada tiro y cada tres estructuras se encuentra una zona de oficinas que es donde se localiza el elevador que conduce a la superficie. Dentro de esta primera zona se localiza una especie de muelle o de dique seco donde se cree que se encontraba el mítico carguero Borealis.
+
+La otra zona, que es la más superficial y que se encuentra sobre el complejo antiguo de laboratorios, es la más nueva, y cuenta con una zona donde se almacenan una serie de habitaciones en donde se encuentran los sujetos de prueba. Estos módulos son almacenados como contenedores, que pueden ser transportados a donde se los necesite. Otro sector de las instalaciones es la zona de fabricación de torretas y almacenamiento de neurotoxinas, así como de cubos de contrapeso y demás elementos útiles para las pruebas. Una de las zonas más profundas de estas instalaciones es el enorme incinerador el cual está conectado por medio de ductos a todos las compuertas de incineración en el complejo.
+
+Finalmente en estas instalaciones nuevas se encuentra la zona de cámaras de prueba, una enorme zona donde se encuentran las estructuras que componen las cámaras de pruebas. Estas cámaras se mueven por rieles, y cuentan con oficinas desde las cuales se podía observar a los sujetos de prueba antes del incidente causado por GLaDOS. En esta zona también se encuentran algunos complejos de oficinas y la cámara principal, donde GLaDOS habita y donde se encuentra la cámara de interruptores, es por esto que esta cámara es el corazón de las instalaciones, ya que desde aquí se controla la totalidad de los laboratorios.
+
+En la zona de la superficie se encuentran los edificios de las oficinas exteriores de los laboratorios y el aparcamiento de los empleados, esto se puede distinguir al final del juego de Portal. En la última escena del juego Portal 2, se ve que un extenso campo de de trigo, el cual se dice que solía ser el mismo lugar del aparcamiento de los empleados, pero más deteriorado, lo cual no se sabe ya que se desconoce la cantidad de tiempo que pasó entre el primer juego y el segundo, aunque al principio de Portal 2 luego de despertar la segunda vez el locutor dice: "Buenos días, has estado criogenizada nueve... nueve, nueve, nueve" y luego se deja de escuchar. Podría estar diciendo que has estado criogenizado 9 años, 9 días, 9 semanas, 9 meses... O bien, podría estar diciendo que has estado criogenizado por tiempo indefinido, ya que las instalaciones han sufrido un gran deterioro por parte de la vegetación y por causa de la falta de mantenimiento por parte de GLaDOS.</p>
+<h2 id="historiah2">Historia</h2> <hr />
+<p id="historia">
+En 1952, Aperture Laboratories comienza sus operaciones como una fabricante de cortinas de baño. Sus productos proveían portales de baja tecnología entre el interior y el exterior de las duchas. En realidad, hay muy poca ciencia en estos productos: el nombre fue elegido para hacer que las cortinas pareciesen más higiénicas. En 1956 la administración de Eisenhower ofrece un contrato a Aperture Laboratories para proveer cortinas de baño a todas las ramas militares, exceptuando la Marina. Durante estos primeros años, en secreto su fundador Cave Johnson realiza en las instalaciones subterráneas, de una antigua mina de sal, el desarrollo de varios experimentos extravagantes que incluyen el uso de los primitivos portales, y haciendo uso de sus relaciones con el gobierno de Estados Unidos recluta soldados y astronautas para realizar esas peligrosas pruebas.
+
+En 1978 el fundador de Aperture Laboratories, Cave Johnson, es expuesto a una intoxicación por polvo de roca lunar en un intento por buscar volverlos gel, descubriendo las propiedades que este gel tenía como conductor de portales intentando que con su nueva tecnología de portales pudiese encontrar una cura a su envenenamiento. Durante esta época la empresa pierde credibilidad y los ejecutivos (Johnson) se ven en la necesidad de usar vagabundos como sujetos de prueba, ya que dejan de tener buenas relaciones con el gobierno de Estados Unidos.
+
+En 1979, los riñones de Johnson dejaron de funcionar. Con el cerebro dañado, agonizando, y siendo incapaz de ser convencido de que el tiempo no está volviendo atrás, Johnson idea un Programa de Tres Fases en el cual los resultados, según sus palabras "garantizarán el continuado éxito de Aperture Laboratories en el distante pasado que rápidamente se acerca". Dichas fases son:
+
+Fase 1: Contramaniobra de Heimlich, una técnica para interrumpir la Maniobra de Heimlich.
+Fase 2: Fundación "Toma un deseo", una organización "caritativa" que compraría deseos a padres de niños con enfermedades terminales y los redistribuiría a adultos con deseos pero saludables.
+Fase 3: En sus palabras, "Alguna especie de rotura en el espacio... que podría ser... bueno, sería algo así, no lo sé, algo que ayudaría con las cortinas de baño supongo. No la he pensado tanto como la de Toma un deseo".
+Tras la muerte de Johnson, la empresa queda en manos de su secretaria Caroline y los laboratorios sufren una escasez de sujetos de prueba y se comienza a obligar a los trabajadores a ocuparse como sujetos de prueba periódicamente. Antes de la muerte de Johnson este estaba pensando en una forma de conservar su personalidad y la de Caroline en una computadora y por la escasez de sujetos de prueba en construir robots para sustituirlos.
+
+En 1981 los dirigentes ingenieros de Aperture Laboratories completan las iniciativas de la Contramaniobra de Heimlich y la Fundación "Toma un deseo". Los productos son anunciados en una prodigiosa ceremonia televisada. Las creaciones se volvieron inmediata y salvajemente impopulares. Después de una sucesión de desastres de niños enfermos, los ejecutivos de Aperture Laboratories son convocados ante un comité investigador del Senado. Durante este comité, un ingeniero menciona que se ha progresado en el desarrollo de la Fase 3: "Un túnel de tamaño humano a través del espacio físico con tantas aplicaciones posibles como una cortina de baño". El comité es rápidamente cerrado y Aperture Laboratories es premiado con un contrato indefinido para continuar secretamente con los proyectos de portales y de la Contramaniobra de Heimlich.
+
+En 1986 llega a los oídos de los ejecutivos de Aperture Laboratories que otro contratista de Defensa está trabajando en una tecnología de portales similar: Black Mesa. Al parecer, están teniendo más éxito por la tecnología que han podido "reciclar", dada su ubicación en una instalación de misiles balísticos ahora rediseñada como laboratorio. En respuesta a esto comienza el desarrollo de la "Forma de vida genética y sistema operativo de disco" o GLaDOS (en inglés Genetic Lifeform and Disk Operating System), un ayudante de desarrollo con inteligencia artificial, siendo parte de la idea que Johnson había concebido de "guardar" su personalidad y la de su secretaria en una computadora.
+
+En 1996, después de una década concentrándose en darles a las partes de GLaDOS una mayor funcionalidad básica, se comienza a trabajar en los componentes de forma de vida genética. Años después, la inteligencia artificial, tras varios intentos fallidos de matar a los científicos, es activada por primera vez el día anual de "Trae a tu hija al trabajo", una de las actividades organizadas por Aperture Laboratories. Al comienzo la prueba es exitosa; sin embargo la IA logra su cometido y causa la muerte de la mayoría de los científicos inundando los laboratorios de neurotoxinas.
+
+Después de estos acontecimientos el estatus de los laboratorios se vuelve desconocido y no se sabe si está legalmente en funcionamiento. Lo que se sabe es que GLaDOS ha continuado los experimentos con el arma creadora de portales tal vez en un intento de deshacerse de todos los sujetos de prueba, antes de usar a los nuevos robots creados por ella y uno de los sueños que Johnson pensó años antes.</p>
+</section>
+<aside><img id="imgRetrato" src="imagenes/aside.jpg" alt="">
+<p id="retrato">Cave Jhonson Fundador</p></aside>
+</div>
+<footer>Don't look for us here: 46°14′00″N 86°21′00″O</footer>
+</body>
+</html>
